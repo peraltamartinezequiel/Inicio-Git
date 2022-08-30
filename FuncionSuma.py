ef1ad4e1cb8025c289a53ee3d5ccdfec86461615
peraltamartinezequiel <peraltamartinezequiel@hotmail.com>
2022-08-29 22:41:19 -0300

    Programa simple de Python. GIT

---
+++ FuncionSuma.py
@@ -0,0 +1,5 @@
+def suma (num1, num2):
+    return num1 + num2
+num1 = int(input('Ingrese un numero: '))
+num2 = int(input('Ingrese otro numero: '))
+print('El resultado de sumar num1 con num 2 es:', suma(num1, num2), '.')
